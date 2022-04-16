ee78c05cc72f46c6abaf907f59b5159157178068
Adophilus <uchenna19of@gmail.com>
2022-04-16 16:30:31 +0100

    need to fix jumping

--- src/Sprite.js
+++ src/Sprite.js
@@ -1,6 +1,8 @@
 import AttackBox from './AttackBox.js'
 
 class Sprite {
+  static ID = 0
+
   WIDTH = 25
   HEIGHT = 50
   VEL = 7
@@ -31,8 +33,18 @@ class Sprite {
     this.VEL = vel ? vel : this.VEL
     this.CONTROLS = controls ? controls : this.CONTROLS
     this.attackBox = new AttackBox({ sprite: this, game })
+    this.assignId()
 
     this.hookKeys()
+  }
+
+  assignId() {
+    this.id = this.constructor.ID
+    this.constructor.ID += 1
+  }
+
+  equals(obj) {
+    return this.id === obj.id
   }
 
   // handle key events
@@ -114,7 +126,8 @@ class Sprite {
 
   moveRight(force = false) {
     if (this.MOVING.right || force) {
-      if (this.position[0] + this.VEL + this.WIDTH <= this.game.WIDTH) {
+      let newPos = this.position[0] + this.VEL
+      if (this.canGo([newPos, this.position[1]])) {
         this.position[0] += this.VEL
       }
       this.FACING = 1
@@ -128,6 +141,45 @@ class Sprite {
     }
   }
 
+  canGo(position) {
+    let objects = new Array(
+      ...this.game.OBJECTS,
+      ...this.game.ENEMIES,
+      ...this.game.PLAYERS
+    )
+    for (let o of objects) {
+      if (this.equals(o)) {
+        continue
+      }
+
+      if (
+        ((position[0] >= o.position[0] &&
+          position[0] <= o.position[0] + o.WIDTH) ||
+          (o.position[0] <= position[0] &&
+            o.position[0] + o.WIDTH >= this.position[0])) &&
+        position[1] <= o.position[1] &&
+        position[1] + this.WIDTH <= o.position[1]
+      ) {
+        return false
+      } else if (
+        position[0] + this.WIDTH >= o.position[0] &&
+        position[0] + this.WIDTH <= o.position[0] + o.WIDTH &&
+        position[1] + this.HEIGHT >= o.position[0] &&
+        position[1] + this.HEIGHT <= o.position[0] + o.HEIGHT
+      ) {
+        return false
+      }
+    }
+
+    if (position[0] >= 0 && position[0] + this.WIDTH <= this.game.WIDTH) {
+      if (position[1] >= 0 && position[1] + this.HEIGHT <= this.game.HEIGHT) {
+        return true
+      }
+    }
+
+    return false
+  }
+
   // handle animation and collisions
   draw() {
     // handle movement
@@ -136,6 +188,15 @@ class Sprite {
     // handle attack
     this.attack()
 
+    // draw collision border
+    this.game.context.fillStyle = 'deepskyblue'
+    this.game.context.fillRect(
+      this.position[0] - 2,
+      this.position[1] - 2,
+      this.WIDTH + 4,
+      this.HEIGHT
+    )
+
     this.game.context.fillStyle = 'red'
     this.game.context.fillRect(...this.position, this.WIDTH, this.HEIGHT)
 
@@ -143,7 +204,10 @@ class Sprite {
     if (this.position[1] + this.HEIGHT < this.game.HEIGHT) {
       let newPos = this.position[1] + this.GRAVITY
 
-      if (newPos < this.game.HEIGHT - this.HEIGHT) {
+      if (
+        newPos < this.game.HEIGHT - this.HEIGHT &&
+        this.canGo([this.position[0], newPos])
+      ) {
         this.position[1] = newPos
       } else {
         this.position[1] = this.game.HEIGHT - this.HEIGHT
