ec20cafb99e4724d91f6c88fdd7b5aca7e4edca9
Adophilus <uchenna19of@gmail.com>
2022-05-03 21:41:02 +0100

    refactored codebase and added mixins

---
+++ src/classes/AttackBox.js
@@ -0,0 +1,41 @@
+class AttackBox {
+  static ID = 100
+  HEIGHT = 20
+  WIDTH = 30
+
+  constructor({ game, sprite }) {
+    this.game = game
+    this.sprite = sprite
+
+    this.position = this.__setPosition()
+    this.__assignId()
+  }
+
+  __assignId() {
+    this.id = ++this.constructor.ID
+  }
+
+  equals(obj) {
+    return this.id === obj.id
+  }
+
+  __setPosition() {
+    if (this.sprite.FACING === 1) {
+      return [
+        this.sprite.position[0] + this.sprite.WIDTH,
+        this.sprite.position[1]
+      ]
+    } else {
+      return [this.sprite.position[0] - this.WIDTH, this.sprite.position[1]]
+    }
+  }
+
+  draw() {
+    this.position = this.__setPosition()
+
+    this.game.context.fillStyle = 'blue'
+    this.game.context.fillRect(...this.position, this.WIDTH, this.HEIGHT)
+  }
+}
+
+export default AttackBox
---
+++ src/classes/Game.js
@@ -0,0 +1,174 @@
+import Sprite from './Sprite.js'
+
+class Game {
+  WIDTH = 500 // 1024
+  HEIGHT = 500 // 576
+  PLAYERS = []
+  ENEMIES = []
+  OBJECTS = []
+
+  constructor() {
+    this.canvas = document.querySelector('#game')
+    this.context = this.canvas.getContext('2d')
+  }
+
+  init() {
+    this.canvas.width = this.WIDTH
+    this.canvas.height = this.HEIGHT
+    this.draw()
+  }
+
+  start() {
+    this.PLAYERS = [
+      new Sprite({
+        position: [100, 0],
+        game: this
+      }),
+      new Sprite({
+        position: [300, 0],
+        game: this,
+        controls: {
+          up: 'ArrowUp',
+          down: 'ArrowDown',
+          left: 'ArrowLeft',
+          right: 'ArrowRight',
+          attack: 'Insert'
+        }
+      })
+    ]
+    this.PLAYERS[0].DEBUG = true
+
+    window.playerHealthBar = this.PLAYERS[0].healthBar
+    this.animate()
+  }
+
+  canGo({ object, position, debug, reason, exclude }) {
+    let objects = new Array(...this.OBJECTS, ...this.ENEMIES, ...this.PLAYERS)
+
+    for (let o of objects) {
+      if (o.equals(object)) {
+        continue
+      }
+      if (exclude) {
+        if (exclude.find((_o) => o.equals(_o))) {
+          continue
+        }
+      }
+
+      if (
+        position[0] + object.WIDTH >= o.position[0] &&
+        position[0] <= o.position[0] + o.WIDTH &&
+        position[1] + object.HEIGHT >= o.position[1] &&
+        position[1] <= o.position[1] + o.HEIGHT
+      ) {
+        if (reason) {
+          return { status: false, reason: o }
+        }
+        return false
+      }
+    }
+
+    if (position[0] < 0) {
+      if (reason) {
+        return {
+          status: false,
+          reason: { width: -1 }
+        }
+      }
+      return false
+    } else if (position[0] + object.WIDTH > this.WIDTH) {
+      if (reason) {
+        return {
+          status: false,
+          reason: { width: this.WIDTH + 1 }
+        }
+      }
+      return false
+    }
+
+    if (position[1] < 0) {
+      if (reason) {
+        return {
+          status: false,
+          reason: { height: -1 }
+        }
+      }
+      return false
+    } else if (position[1] + object.HEIGHT > this.HEIGHT) {
+      if (reason) {
+        return {
+          status: false,
+          reason: { height: this.HEIGHT + 1 }
+        }
+      }
+      return false
+    }
+
+    if (reason) {
+      return {
+        status: true
+      }
+    }
+    return true
+  }
+
+  initiateAttack({ initiator, atk }) {
+    let objects = new Array(...this.OBJECTS, ...this.ENEMIES, ...this.PLAYERS)
+
+    for (let o of objects) {
+      if (initiator.equals(o)) {
+        continue
+      }
+
+      let canStayInPosition = this.canGo({
+        object: initiator.attackBox,
+        position: initiator.attackBox.position,
+        reason: true,
+        exclude: [initiator]
+      })
+      if (
+        !canStayInPosition.status &&
+        canStayInPosition.reason instanceof Sprite
+      ) {
+        canStayInPosition.reason.receiveAttack(atk)
+      }
+    }
+  }
+
+  draw() {
+    this.context.fillStyle = 'black'
+    this.context.fillRect(0, 0, this.canvas.width, this.canvas.height)
+  }
+
+  animate() {
+    this.draw()
+
+    this.OBJECTS.forEach((o, i) => {
+      if (o.HEALTH) {
+        o.draw()
+      } else {
+        this.OBJECTS.pop(i)
+      }
+    })
+
+    this.ENEMIES.forEach((o, i) => {
+      if (o.HEALTH) {
+        o.draw()
+      } else {
+        this.ENEMIES.pop(i)
+      }
+    })
+
+    this.PLAYERS.forEach((o, i) => {
+      if (o.HEALTH) {
+        o.draw()
+      } else {
+        this.PLAYERS.pop(i)
+      }
+    })
+
+    window.requestAnimationFrame(() => this.animate())
+  }
+}
+
+export default Game
